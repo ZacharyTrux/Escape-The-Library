bc33dbeecc20edc1a42a05ef115fdb40775ab014
boomcar127 <elzahremilio@gmail.com>
2026-04-02 19:15:42 -0500

    Button Scripts mostly complete

--- Assets/Scripts/HorrorRoom_ButtonScript.cs
+++ Assets/Scripts/HorrorRoom_ButtonScript.cs
@@ -5,19 +5,38 @@ public class HorrorRoom_ButtonScript : MonoBehaviour
     public int buttonID;
     public HorrorRoom_PuzzleManager puzzleManager;
 
-    private void OnMouseDown()
-    {
-        puzzleManager.PressButton(ID)
-    }
+    private Renderer rend;
+
+    public Color idleColor = Color.blue;
+    public Color correctColor = Color.green;
+    public Color wrongColor = Color.red;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
+        rend.material.color = idleColor;
+    }
+    void OnMouseDown()
+    {
+        puzzleManager.PressButton(buttonID, this);
+        transform.localScale *= 0.9f;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetCorrect()
     {
-        
+        rend.material.color = correctColor;
     }
+
+    public void SetWrong()
+    {
+        rend.material.color = wrongColor;
+    }
+
+    public void ResetColor()
+    {
+        rend.material.color = idleColor;
+    }
+
 }
+
--- Assets/Scripts/HorrorRoom_PuzzleManager.cs
+++ Assets/Scripts/HorrorRoom_PuzzleManager.cs
@@ -1,5 +1,3 @@
-using Tripolygon.UModelerX.Runtime;
-using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 
 public class HorrorRoom_PuzzleManager : MonoBehaviour
@@ -16,7 +14,9 @@ public class HorrorRoom_PuzzleManager : MonoBehaviour
     public PuzzleState currentState = PuzzleState.Idle;
     public HorrorRoom_DoorController door;
 
-    public void PressButton(int buttonID)
+    public HorrorRoom_ButtonScript[] buttons;
+
+    public void PressButton(int buttonID, HorrorRoom_ButtonScript button)
     {
         switch (currentState)
         {
@@ -24,25 +24,31 @@ public class HorrorRoom_PuzzleManager : MonoBehaviour
                 if (buttonID == 1)
                 {
                     currentState = PuzzleState.Step1;
+                    button.SetCorrect();
                 }
                 else
-                    Fail();
+                    Fail(button);
                 break;
 
             case PuzzleState.Step1:
                 if (buttonID == 2)
                 {
-                    currentState = PuzzleState.Step1;
+                    currentState = PuzzleState.Step2;
+                    button.SetCorrect();
                 }
                 else
-                    Fail();
+                    Fail(button);
                 break;
 
             case PuzzleState.Step2:
                 if (buttonID == 3)
+                {
+                    button.SetCorrect();
                     Solve();
+                }
+                    
                 else
-                    Fail();
+                    Fail(button);
                 break;
         }
     }
@@ -50,13 +56,16 @@ public class HorrorRoom_PuzzleManager : MonoBehaviour
     void Solve()
     {
         currentState = PuzzleState.Solved;
-        door.OpenDoor();
+        if (door != null) door.OpenDoor();
         Debug.Log("Puzzle Solved!");
     }
 
-    void Fail()
+    void Fail(HorrorRoom_ButtonScript wrongButton)
     {
         currentState = PuzzleState.Failed;
+
+        if (wrongButton != null) wrongButton.SetWrong();
+
         Debug.Log("Wrong order! Resetting...");
         Invoke(nameof(ResetPuzzle), 2f);
     }
@@ -64,6 +73,11 @@ public class HorrorRoom_PuzzleManager : MonoBehaviour
     void ResetPuzzle()
     {
         currentState = PuzzleState.Idle;
+
+        foreach (var b in buttons)
+        {
+            b.ResetColor();
+        }
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
